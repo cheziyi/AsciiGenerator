aab99446505b44e2c61068ad527f5c1c946cc299
Calvin Che <calvin@cczy.io>
2018-06-28 17:52:03 +0800

    Add project files.

---
+++ AsciiGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsciiGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
